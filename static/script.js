77208773575133e0a5003f2ab4e30349d2490632
Sunny Capt <sunny.capt@tuta.io>
2019-10-19 04:37:31 +0300

    added github repo cards

--- static/script.js
+++ static/script.js
@@ -1,8 +1,8 @@
 function runSounds(){
   Array('psx.wav', 'boon.wav').forEach(v=>{
-	 var audio=new Audio('/static/sound/' + v);
-	 audio.loop=true;
-   audio.autoplay=true;
+	 var audio = new Audio('/static/sound/' + v);
+	 audio.loop = true;
+   audio.autoplay = true;
 	 audio.play().catch(err=>{document.getElementById('gf1').addEventListener('click', runSounds)});
    document.getElementById('gf1').removeEventListener('click', runSounds);
   });
@@ -15,7 +15,26 @@ async function setPGPpk(){
   pgpArticle.innerText = key;
 }
 
+function showRepo(jsonRepo){
+  let tmp = document.createElement('div');
+  tmp.classList.add('blacked', 'repo');
+  tmp.innerHTML = '<div class="link">' + 
+                    `<a href="${jsonRepo.html_url}">${jsonRepo.name}</a>` +
+                  '</div>' +
+                  '<div class="details">' +
+                    jsonRepo.description
+                  '</div>';
+  document.getElementById('projects').append(tmp);
+}
+
+async function setRepos(userName){
+  let response = await fetch(`//api.github.com/users/${userName}/repos`);
+  let jsonRepos = await response.json();
+  jsonRepos.forEach(jsonRepo=>showRepo(jsonRepo));
+}
+
 function onLoad(){
   setPGPpk();
+  setRepos('SunnyCapt');
   runSounds();
 };
